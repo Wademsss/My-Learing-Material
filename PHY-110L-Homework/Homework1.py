d20ae05cf396127f4b403a0590cca3de023c5d49
Goooooose <wademss@outlook.com>
2026-04-13 15:49:17 -0700

    Marked name on the assignment.

--- PHY-110L-Homework/Homework1.py
+++ PHY-110L-Homework/Homework1.py
@@ -1,3 +1,6 @@
+# PHY 110L Homework 1
+# Yucheng Liu
+# 919908992
 # AI clarifies: In this project, GitHub Colpiolt was used as a code completion tool.
 import numpy as np
 import matplotlib.pyplot as plt
